20d45a203bbe21508c1be328e31fb7788744e761
liuleyou <707649841@qq.com>
2019-08-16 20:16:00 +0800

    案例

---
+++ Assets/Scenes/simple2/myParalleForJobsTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+using Unity.Collections;
+struct myParalleForJobs : IJobParallelFor
+{
+    [ReadOnly]
+    public float a;
+    [ReadOnly]
+    public float b;
+    public NativeArray<float> result;
+
+    public void Execute(int index)
+    {
+        result[index] = index + a + b;
+    }
+   
+}
+public class myParalleForJobsTest : MonoBehaviour
+{
+  
+    // Update is called once per frame
+    void Update()
+    {
+        NativeArray<float> array = new NativeArray<float>(2,Allocator.TempJob);
+        myParalleForJobs job = new myParalleForJobs();
+        job.a = 1;
+        job.b = 3;
+        job.result = array;
+
+        JobHandle handle = job.Schedule(2,2);
+        handle.Complete();
+        var data = job.result[1];
+        Debug.LogError(data);
+
+        array.Dispose();
+    }
+}
---
+++ Assets/Scenes/simple3/myIJobParallelForTransformTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+using Unity.Collections;
+using UnityEngine.Jobs;
+
+struct  myIJobParallelForTransform :  IJobParallelForTransform
+{
+    [ReadOnly]
+    public float a;
+    [ReadOnly]
+    public float b;
+    public NativeArray<float> result;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+        var pos = transform.position;
+        pos.x = a + b;
+        transform.position = pos;
+        result[index] = pos.x;
+    }
+}
+public class myIJobParallelForTransformTest : MonoBehaviour
+{
+   public Transform[] tlist;
+    // Update is called once per frame
+    void Update()
+    {
+        NativeArray<float> array = new NativeArray<float>(tlist.Length,Allocator.TempJob);
+        myIJobParallelForTransform job = new myIJobParallelForTransform();
+        job.a = 1;
+        job.b = 3;
+        job.result = array;
+
+        TransformAccessArray ta = new TransformAccessArray(tlist);
+        JobHandle handle = job.Schedule(ta);
+        handle.Complete();
+        var data = job.result[1];
+        Debug.LogError(data);
+
+        array.Dispose();
+        ta.Dispose();
+    }
+}
